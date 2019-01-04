615329c31c7d9dff3d22b08c8bda6e82928c0e69
lzcdev <lzc2134@gmail.com>
2019-01-04 18:01:39 +0800

    调整

---
+++ baike_spider_2.7/html_downloader.py
@@ -0,0 +1,21 @@
+
+import urllib2
+
+class HtmlDownloader(object):
+
+    def download(self, url):
+        if url is None:
+            print 'url is None'
+            return  None
+
+        try:
+            response = urllib2.urlopen(url, timeout=10)
+            if response.getcode() != 200:
+                print 'false'
+                return None
+            print 'success'
+        except:
+            print 'timeout'
+        # print response.read()
+        return response.read()
+
---
+++ baike_spider_2.7/html_parser.py
@@ -0,0 +1,48 @@
+
+from bs4 import BeautifulSoup
+import urlparse
+import re
+
+
+class HtmlParser(object):
+
+    def _get_new_urls(self, page_url, soup):
+        new_urls = set()
+        # /view/123.html
+        # /item/Python/407313
+        # links = soup.find_all('a', href=re.compile(r"/view/\d+\.htm"))
+        links = soup.find_all('a', href=re.compile(r"/item/*"))
+
+        for link in links:
+            new_url = link['href']
+            new_full_url = urlparse.urljoin(page_url, new_url)
+            new_urls.add(new_full_url)
+        return new_urls
+
+
+    def _get_new_data(self, page_url, soup):
+        res_data = {}
+
+        res_data['url'] = page_url
+# <dd class="lemmaWgt-lemmaTitle-title"> <h1>Python</h1>
+
+        title_node = soup.find('dd', class_="lemmaWgt-lemmaTitle-title").find("h1")
+        res_data['title'] = title_node.get_text()
+        print res_data['title']
+        # class = "lemma-summary" label-module="lemmaSummary" >
+        # summary_node = soup.find('div', classs_="lemma-summary")
+        # res_data['summary'] = summary_node.get_text()
+
+        return res_data
+
+
+    def parse(self, page_url, html_cont):
+        if page_url is None or html_cont is None:
+            print 'page_url is None'
+            return
+
+        soup = BeautifulSoup(html_cont, 'html.parser', from_encoding='utf-8')
+        new_urls = self._get_new_urls(page_url, soup)
+        new_data = self._get_new_data(page_url, soup)
+        return new_urls, new_data
+
