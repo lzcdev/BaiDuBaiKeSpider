3b88b57b7fd8131b9c879d95c8af662203c0c82a
lzcdev <lzc2134@gmail.com>
2019-01-04 18:02:06 +0800

    增加Python3.7版本

---
+++ baike_spider_3.7/html_downloader.py
@@ -0,0 +1,21 @@
+
+import urllib.request
+
+class HtmlDownloader(object):
+
+    def download(self, url):
+        if url is None:
+            print ('url is None')
+            return  None
+
+        try:
+            response = urllib.request.urlopen(url, timeout=10)
+            if response.getcode() != 200:
+                print ('false')
+                return None
+            print ('success')
+        except:
+            print ('timeout')
+        # print response.read()
+        return response.read()
+
---
+++ baike_spider_3.7/html_parser.py
@@ -0,0 +1,55 @@
+
+from bs4 import BeautifulSoup
+# import urlparse
+# from urllib import parse
+from urllib.parse import urljoin
+import re
+
+
+class HtmlParser(object):
+
+    def _get_new_urls(self, page_url, soup):
+        new_urls = set()
+        # /view/123.html
+        # /item/Python/407313
+        # links = soup.find_all('a', href=re.compile(r"/view/\d+\.htm"))
+        links = soup.find_all('a', href=re.compile(r"/item/*"))
+
+        for link in links:
+            new_url = link['href']
+            new_full_url = urljoin(page_url, new_url)
+            new_urls.add(new_full_url)
+        return new_urls
+
+
+    def _get_new_data(self, page_url, soup):
+        res_data = {}
+
+        res_data['url'] = page_url
+# <dd class="lemmaWgt-lemmaTitle-title"> <h1>Python</h1>
+
+        title_node = soup.find('dd', class_="lemmaWgt-lemmaTitle-title").find("h1")
+        res_data['title'] = title_node.get_text()
+        print (res_data['title'])
+        # class = "lemma-summary" label-module="lemmaSummary" >
+        # summary_node = soup.find('div', classs_="lemma-summary")
+        # res_data['summary'] = summary_node.get_text()
+
+        # 目录下所有内容
+        catlog_node = soup.select('div[class="para"]')
+        for content in catlog_node:
+            print(content.get_text().strip())
+
+        return res_data
+
+
+    def parse(self, page_url, html_cont):
+        if page_url is None or html_cont is None:
+            print ('page_url is None')
+            return
+
+        soup = BeautifulSoup(html_cont, 'html.parser', from_encoding='utf-8')
+        new_urls = self._get_new_urls(page_url, soup)
+        new_data = self._get_new_data(page_url, soup)
+        return new_urls, new_data
+
