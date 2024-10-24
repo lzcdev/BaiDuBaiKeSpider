b1aa926c182aaad2868f36e6695dfeb19cd76f0f
lzcdev <jinqing.lzc@raycloud.com>
2024-10-24 11:29:25 +0800

    fix: update parser

--- baike_spider_3.7/html_parser.py
+++ baike_spider_3.7/html_parser.py
@@ -26,9 +26,8 @@ class HtmlParser(object):
         res_data = {}
 
         res_data['url'] = page_url
-# <dd class="lemmaWgt-lemmaTitle-title"> <h1>Python</h1>
-
-        title_node = soup.find('dd', class_="lemmaWgt-lemmaTitle-title").find("h1")
+        # <div class="lemmaTitleBox__OVrj"><h1 class="lemmaTitle_qmNnR J-lemma-title">Python</h1> ...
+        title_node = soup.find('div', class_="lemmaTitleBox__OVrj").find("h1")
         res_data['title'] = title_node.get_text()
         print (res_data['title'])
         # class = "lemma-summary" label-module="lemmaSummary" >
